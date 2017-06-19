b34e304d9f0e3ecaa7cc1dd5d7f60694ece10758
seungah <fofo2525@naver.com>
2017-06-19 19:17:47 +0900

    #3 Event Programming(Switch)

--- app/src/main/java/starbloom0128/kr/hs/emirim/audiotest/MainActivity.java
+++ app/src/main/java/starbloom0128/kr/hs/emirim/audiotest/MainActivity.java
@@ -1,13 +1,35 @@
 package starbloom0128.kr.hs.emirim.audiotest;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+    Switch switchMusic;
+    MediaPlayer musicPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        switchMusic = (Switch)findViewById(R.id.switch_music);
+        musicPlayer=MediaPlayer.create(this, R.raw.open);
+        switchMusic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(switchMusic.isChecked())
+                    musicPlayer.start(); //true면 재생
+                else
+                    musicPlayer.pause(); //일시정지
+            }
+        });
     }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        musicPlayer.stop();
+    }
+
 }
